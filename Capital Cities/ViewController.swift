9bd136425b278713bf5fda4d99a68cc6c68318c3
AKBarcenas <akbarcen@ucsd.edu>
2016-06-27 05:06:28 -0700

    Allow the user to get more information about the capitals.

--- Capital Cities/ViewController.swift
+++ Capital Cities/ViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 import MapKit
 
 class ViewController: UIViewController, MKMapViewDelegate {
+    // The map view that will display the map.
     @IBOutlet weak var mapView: MKMapView!
+    
+    /*
+     * Function Name: viewDidLoad
+     * Parameters: None
+     * Purpose: This method creates annotations for some capital cities and places them onto a map.
+     * Return Value: None
+     */
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        mapView.delegate = self
         
         let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olympics.")
         let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "Founded over a thousand years ago.")
@@ -29,6 +39,62 @@ class ViewController: UIViewController, MKMapViewDelegate {
         // Dispose of any resources that can be recreated.
     }
 
-
+    /*
+     * Function Name: mapView
+     * Parameters: mapView - the map view that called this method.
+     *   viewForAnnotation - the annotation that will be displayed on the map.
+     * Purpose: This method checks the type of annotation that is being displayed. For capital
+     *   annotations it ether tries to reuse another unused annotation view or create a new one.
+     * Return Value: MKAnnotationView!
+     */
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        // 1
+        let identifier = "Capital"
+        
+        // 2
+        if annotation is Capital {
+            // 3
+            var annotationView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier)
+            
+            if annotationView == nil {
+                //4
+                annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView!.canShowCallout = true
+                
+                // 5
+                let btn = UIButton(type: .DetailDisclosure)
+                annotationView!.rightCalloutAccessoryView = btn
+            } else {
+                // 6
+                annotationView!.annotation = annotation
+            }
+            
+            return annotationView
+        }
+        
+        // 7
+        return nil
+    }
+    
+    /*
+     * Function Name: mapView
+     * Parameters: mapView - the map view that called this method.
+     *   view - the annotation that had its button tapped.
+     *   control - the control that was tapped in the view.
+     * Purpose: This method displays an alert controller with the title of the capital and some
+     *   information about the capital.
+     * Return Value: None
+     */
+    
+    func mapView(mapView: MKMapView!, annotationView view: MKAnnotationView!, calloutAccessoryControlTapped control: UIControl!) {
+        let capital = view.annotation as! Capital
+        let placeName = capital.title
+        let placeInfo = capital.info
+        
+        let ac = UIAlertController(title: placeName, message: placeInfo, preferredStyle: .Alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+        presentViewController(ac, animated: true, completion: nil)
+    }
 }
 
